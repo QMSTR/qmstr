8995405d05849780cef340bf29edcab91d2e1720
Nikolas Sepos <nikolas.sepos@gmail.com>
2018-02-02 01:21:39 +0100

    Vendor dependencies with dep
    Run `dep ensure` (only works in GOPATH src context) to get pinned
    version dependencies.

--- install.sh
+++ install.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 go get -u github.com/golang/protobuf/protoc-gen-go
+go get -u github.com/golang/dep/cmd/dep
 
-(go get github.com/QMSTR/qmstr/cmd/qmstr-master || go generate github.com/QMSTR/qmstr/cmd/qmstr-master; go get github.com/QMSTR/qmstr/cmd/qmstr-master)
+go get github.com/QMSTR/qmstr
+(cd $GOPATH/src/github.com/QMSTR/qmstr; dep ensure)
+go generate github.com/QMSTR/qmstr/cmd/qmstr-master
 
-go get github.com/QMSTR/qmstr/cmd/qmstr-wrapper
-go get github.com/QMSTR/qmstr/cmd/qmstr-client
+go install github.com/QMSTR/qmstr/cmd/qmstr-master
+go install github.com/QMSTR/qmstr/cmd/qmstr-wrapper
+go install github.com/QMSTR/qmstr/cmd/qmstr-client
