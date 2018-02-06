9ede9bb21d3a7c74c1bc0e373172b98d7d3f3515
Giasemi Seisa <giasemi.seisa@endocode.com>
2018-02-06 16:44:01 +0100

    Use cobra golang for the client connection to qmstr server.
    qmstr-cli just connects to qmstr server and it can print the version
    and quit qmstr.

---
+++ pkg/cli/root.go
@@ -0,0 +1,50 @@
+package cli
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	pb "github.com/QMSTR/qmstr/pkg/buildservice"
+	"github.com/spf13/cobra"
+	"google.golang.org/grpc"
+)
+
+var conn *grpc.ClientConn
+var buildServiceClient pb.BuildServiceClient
+
+const (
+	address = "localhost:50051"
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "qmstr-cli",
+	Short: "Qmstr client connects to qmstr and serves",
+	Long: `Qmstr client connects to qmstr and serves depending 
+	what we want to do with it. Right now it just quits the server 
+	and prints the version of qmstr-cli.`,
+	Run: func(cmd *cobra.Command, args []string) {
+	},
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func setUpServer() {
+	// Set up server connection
+	var err error
+	conn, err = grpc.Dial(address, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("Failed to connect to master: %v", err)
+	}
+	buildServiceClient = pb.NewBuildServiceClient(conn)
+}
+
+func tearDownServer() {
+	conn.Close()
+}
