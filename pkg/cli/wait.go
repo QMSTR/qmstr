2aa610b9fac0e62cb03eab055e77ac0c9eeb3787
Giasemi Seisa <giasemi.seisa@endocode.com>
2018-02-07 14:23:39 +0100

    Insert wait command in qmstr-client
    qmstr-client waits until the qmstr server is ready.
    We need this so as to know if can start the analysis.

---
+++ pkg/cli/wait.go
@@ -0,0 +1,49 @@
+package cli
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	pb "github.com/QMSTR/qmstr/pkg/buildservice"
+	"github.com/spf13/cobra"
+	"golang.org/x/net/context"
+)
+
+var timeout int
+
+var waitCmd = &cobra.Command{
+	Use:   "wait",
+	Short: "waits until the server is ready",
+	Long: `waits until the server is ready. If 60 seconds pass 
+	then it exits with error message.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("wait called")
+		setUpServer()
+		awaitServer()
+		tearDownServer()
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(waitCmd)
+	waitCmd.Flags().IntVarP(&timeout, "timeout", "t", 60, "time is up")
+
+}
+
+func awaitServer() {
+	go func() {
+		<-time.After(time.Second * time.Duration(timeout))
+		os.Exit(2)
+	}()
+	for {
+		res, err := buildServiceClient.Log(context.Background(), &pb.LogMessage{Msg: []byte("Client is waiting for qmstr server to be ready")})
+		if err != nil {
+			continue
+		}
+		if res.Success {
+			return
+		}
+	}
+
+}
