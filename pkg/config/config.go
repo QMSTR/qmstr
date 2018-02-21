ce662a82e5835a7e6302a5a71d2332aef5cae7ed
Markus Herpich <markus@endocode.com>
2018-02-21 15:19:31 +0100

    Add configuration package
    Allow reading configuration from a YAML file.

---
+++ pkg/config/config.go
@@ -0,0 +1,74 @@
+package config
+
+import (
+	"io/ioutil"
+	"log"
+	"os"
+
+	yaml "gopkg.in/yaml.v2"
+)
+
+type PathSubstitution struct {
+	Old string
+	New string
+}
+
+type Analysis struct {
+	Selector string
+	Analyzer string
+	PathSub  []*PathSubstitution
+	Config   map[string]string
+}
+
+type Reporting struct {
+	Selector   string
+	Name       string
+	ReportType string
+}
+
+type ServerConfig struct {
+	RPCAddress string
+	DBAddress  string
+	DBWorkers  int
+}
+
+type MasterConfig struct {
+	Server    *ServerConfig
+	Analysis  []Analysis
+	Reporting []Reporting
+}
+
+func getDefaultConfig() *MasterConfig {
+	return &MasterConfig{
+		Server: &ServerConfig{DBWorkers: 2, RPCAddress: ":50051", DBAddress: "localhost:9080"},
+	}
+}
+
+func ReadConfigFromFile(configfile string) (*MasterConfig, error) {
+	log.Printf("Reading configuration from %s\n", configfile)
+	data, err := ConsumeFile(configfile)
+	if err != nil {
+		return nil, err
+	}
+
+	configuration := getDefaultConfig()
+
+	err = yaml.Unmarshal(data, configuration)
+	if err != nil {
+		return nil, err
+	}
+
+	return configuration, nil
+}
+
+func ConsumeFile(filename string) ([]byte, error) {
+	f, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	data, err := ioutil.ReadAll(f)
+	if err != nil {
+		return nil, err
+	}
+	return data, nil
+}
