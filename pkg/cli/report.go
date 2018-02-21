4786ad1f3bb53e80002d1305950ef3362544488d
Markus Herpich <markus@endocode.com>
2018-02-21 15:21:22 +0100

    Adapt CLI to new Analyze and Report functionallity

--- pkg/cli/report.go
+++ pkg/cli/report.go
@@ -2,13 +2,14 @@ package cli
 
 import (
 	"fmt"
+	"io"
+	"log"
 	"os"
 
 	"golang.org/x/net/context"
 
 	"github.com/QMSTR/qmstr/pkg/buildservice"
 	"github.com/spf13/cobra"
-	"gopkg.in/yaml.v2"
 )
 
 var reportCmd = &cobra.Command{
@@ -17,16 +18,8 @@ var reportCmd = &cobra.Command{
 	Long:  `Generate report described in provided YAML file on the master server.`,
 	Run: func(cmd *cobra.Command, args []string) {
 
-		if len(args) != 1 {
-			fmt.Println("Please provide one yaml file")
-			os.Exit(ReturnCodeParameterError)
-		}
-
-		repMsg := &buildservice.ReportMessage{}
-
 		setUpServer()
-		unmarshalReportRequest(args[0], repMsg)
-		submitReportRequest(repMsg)
+		submitReportRequest()
 		tearDownServer()
 	},
 }
@@ -35,29 +28,22 @@ func init() {
 	rootCmd.AddCommand(reportCmd)
 }
 
-func submitReportRequest(repMsg *buildservice.ReportMessage) {
-	resp, err := buildServiceClient.Report(context.Background(), repMsg)
+func submitReportRequest() {
+	bsrStream, err := buildServiceClient.Report(context.Background(), &buildservice.ReportMessage{Async: false})
 	if err != nil {
 		fmt.Printf("Failed to communicate with qmstr-master server. %v\n", err)
 		os.Exit(ReturnCodeServerCommunicationError)
 	}
-	if !resp.Success {
-		fmt.Println("Server responded unsuccessful")
-		os.Exit(ReturnCodeResponseFalseError)
-	}
-	fmt.Println(resp.ResponseMessage)
-}
 
-func unmarshalReportRequest(yamlFile string, request *buildservice.ReportMessage) {
-	data, err := consumeFile(yamlFile)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(ReturnCodeSysError)
-	}
-
-	err = yaml.Unmarshal(data, &request)
-	if err != nil {
-		fmt.Printf("Wrong YAML file format %v", err)
-		os.Exit(ReturnCodeFormatError)
+	for {
+		resp, err := bsrStream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Printf("Report failed %v", err)
+			os.Exit(ReturnCodeServerCommunicationError)
+		}
+		fmt.Printf("[%s] %s\n", resp.Reporter, resp.ResponseMessage)
 	}
 }
