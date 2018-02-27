d85d0798638537672ee22057899028f3ac405523
Mirko Boehm (Endocode) <mirko@endocode.com>
2018-02-27 15:56:25 +0100

    Starting, creating skeleton for the HTML reporter.

---
+++ pkg/reporter/htmlreporter/client.go
@@ -0,0 +1,37 @@
+package htmlreporter
+
+import (
+	"context"
+	"log"
+
+	pb "github.com/QMSTR/qmstr/pkg/buildservice"
+	"google.golang.org/grpc"
+)
+
+const (
+	address = "localhost:50051"
+)
+
+var conn *grpc.ClientConn
+var buildServiceClient pb.BuildServiceClient
+
+// ConnectToMaster connects to the QMSTR master process.
+func ConnectToMaster() {
+	// Set up server connection
+	var err error
+	conn, err = grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock())
+	if err != nil {
+		log.Fatalf("Failed to connect to master: %v", err)
+	}
+	buildServiceClient = pb.NewBuildServiceClient(conn)
+}
+
+// Temp blabla
+func Temp() {
+	buildServiceClient.Log(context.Background(), &pb.LogMessage{Msg: []byte("HTML Reporter starting...")})
+}
+
+// DisconnectFromMaster disconnects from the QMSTR master process.
+func DisconnectFromMaster() {
+	conn.Close()
+}
