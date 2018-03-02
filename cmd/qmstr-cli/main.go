c00bbbd9e2cf132b7a6526776f0b60c3e8f161e5
Markus Herpich <markus@endocode.com>
2018-03-02 16:43:11 +0100

    adapt generate instructions

--- cmd/qmstr-cli/main.go
+++ cmd/qmstr-cli/main.go
@@ -1,4 +1,4 @@
-//go:generate protoc -I ../../pkg/service --go_out=plugins=grpc:../../pkg/service ../../pkg/service/*.proto
+//go:generate protoc -I ../../pkg/service --go_out=plugins=grpc:../../pkg/service ../../pkg/service/controlservice.proto
 package main
 
 import (
