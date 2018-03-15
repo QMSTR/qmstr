89b97f4915fca870e86678082680cb99748822ec
Mirko Boehm (Endocode) <mirko@endocode.com>
2018-03-15 02:01:00 +0100

    Add logic to create and delete a temporary Hugo working directory.

--- cmd/qmstr-reporter-html/main.go
+++ cmd/qmstr-reporter-html/main.go
@@ -3,6 +3,7 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
 	"os/exec"
@@ -15,15 +16,30 @@ import (
 //"github.com/endocode/qmstr/pkg/reporter/htmlreporter"
 
 const minimumHugoVersion = "0.32"
+const themeDirectory = "/usr/share/qmstr/reporter-html/theme"
 
 func main() {
-	if detectedVersion, err := DetectHugoAndVerifyVersion(); err != nil {
-		log.Printf("error generating HTML reports: %v", err)
-	} else {
-		log.Printf("detected beautiful Hugo version %v", detectedVersion)
+	detectedVersion, err := DetectHugoAndVerifyVersion()
+	if err != nil {
+		log.Fatalf("error generating HTML reports: %v", err)
 	}
+	log.Printf("detected beautiful Hugo version %v", detectedVersion)
+
 	// htmlreporter.ConnectToMaster()
-	// htmlreporter.Temp()
+	wd, err := CreateHugoWorkingDirectory()
+	if err != nil {
+		log.Fatalf("error preparing temporary Hugo working directory: %v", err)
+	}
+	log.Printf("created temporary Hugo working directory in %v", wd)
+	defer func() {
+		//TODO is there a --keep option or environment variable to keep temporary files?
+		log.Printf("deleting temporary Hugo working directory in %v", wd)
+		if err := os.RemoveAll(wd); err != nil {
+			log.Printf("warning - error deleting temporary Hugo working directory in %v: %v", wd, err)
+		}
+	}()
+	// generate the content data (markdown and JSON) from the master data model
+	// execute Hugo to generate documentation into the specified output directory
 	// defer htmlreporter.DisconnectFromMaster()
 }
 
@@ -77,4 +93,14 @@ func CheckMinimumRequiredVersion(versionstring string) error {
 		return fmt.Errorf("the Quartermaster HTML reporter requires at least Hugo version %v (version %v found)", minimumVersion, detectedVersion)
 	}
 	return nil
+}
+
+//CreateHugoWorkingDirectory creates a temporary directory with the directory structure required to run Hugo
+func CreateHugoWorkingDirectory() (string, error) {
+	tmpWorkDir, err := ioutil.TempDir("", "qmstr-")
+	if err != nil {
+		return tmpWorkDir, fmt.Errorf("error crrating temporary Hugo working directory: %v", err)
+	}
+	//TODO populate working directory with a site template and the theme
+	return tmpWorkDir, nil
 }
