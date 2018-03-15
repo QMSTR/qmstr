3f007a6b2c0b9506f3bbca156b44f32ef35f1982
Mirko Boehm (Endocode) <mirko@endocode.com>
2018-03-15 01:17:42 +0100

    Implement the minimum Hugo version check. Add test.

--- cmd/qmstr-reporter-html/main.go
+++ cmd/qmstr-reporter-html/main.go
@@ -8,13 +8,19 @@ import (
 	"os/exec"
 	"regexp"
 	"strings"
+
+	version "github.com/hashicorp/go-version"
 )
 
 //"github.com/endocode/qmstr/pkg/reporter/htmlreporter"
 
+const minimumHugoVersion = "0.32"
+
 func main() {
-	if _, err := DetectHugoAndVerifyVersion(); err != nil {
+	if detectedVersion, err := DetectHugoAndVerifyVersion(); err != nil {
 		log.Printf("error generating HTML reports: %v", err)
+	} else {
+		log.Printf("detected beautiful Hugo version %v", detectedVersion)
 	}
 	// htmlreporter.ConnectToMaster()
 	// htmlreporter.Temp()
@@ -28,20 +34,23 @@ func DetectHugoAndVerifyVersion() (string, error) {
 	if err != nil {
 		log.Printf("Command finished with error: %v", err)
 		log.Printf("INFO - PATH is %v", os.Getenv("PATH"))
-		return "", fmt.Errorf("Hugo not detected, aborting")
+		return "", fmt.Errorf("Hugo not detected")
 	}
-	return ParseAndCheckVersion(output)
+	version, err := ParseVersion(output)
+	if err != nil {
+		return version, err
+	}
+	return version, CheckMinimumRequiredVersion(version)
 }
 
-// ParseAndCheckVersion returns the version for both released and self-compiled versions
-func ParseAndCheckVersion(output []byte) (string, error) {
+// ParseVersion returns the version for both released and self-compiled versions
+func ParseVersion(output []byte) (string, error) {
 	// is this a version built from a repository?
 	re := regexp.MustCompile("Site Generator v(.+)-(.+) .+/.+ BuildDate")
 	match := re.FindSubmatch(output)
 	if match != nil {
 		version := strings.TrimSpace(string(match[1][:]))
-		tag := strings.TrimSpace(string(match[2][:]))
-		log.Printf("Detected Hugo %v-%v", version, tag)
+		// tag := strings.TrimSpace(string(match[2][:]))
 		return version, nil
 	}
 
@@ -49,8 +58,23 @@ func ParseAndCheckVersion(output []byte) (string, error) {
 	match = re.FindSubmatch(output)
 	if match != nil {
 		version := strings.TrimSpace(string(match[1][:]))
-		log.Printf("Detected Hugo release %v", version)
 		return version, nil
 	}
 	return "", fmt.Errorf(" Unable to parse Hugo version in \"%v\"", string(output[:]))
+}
+
+// CheckMinimumRequiredVersion compares the detected Hugo version with the minimum requirement
+func CheckMinimumRequiredVersion(versionstring string) error {
+	detectedVersion, err := version.NewVersion(versionstring)
+	if err != nil {
+		return fmt.Errorf("unable to parse \"%v\" as a Hugo version", versionstring)
+	}
+	minimumVersion, err := version.NewVersion(minimumHugoVersion)
+	if err != nil {
+		return fmt.Errorf("unable to parse minimum required version \"%v\" as a version (this should not happen)", minimumVersion)
+	}
+	if detectedVersion.LessThan(minimumVersion) {
+		return fmt.Errorf("the Quartermaster HTML reporter requires at least Hugo version %v (version %v found)", minimumVersion, detectedVersion)
+	}
+	return nil
 }
