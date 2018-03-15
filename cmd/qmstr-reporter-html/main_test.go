c2d9367b7c7bed6bfb533a2a01e66109de6a4e34
Mirko Boehm (Endocode) <mirko@endocode.com>
2018-03-15 01:17:42 +0100

    Implement the minimum Hugo version check. Add test.

--- cmd/qmstr-reporter-html/main_test.go
+++ cmd/qmstr-reporter-html/main_test.go
@@ -9,14 +9,22 @@ import (
 func TestParseVersions(t *testing.T) {
 	{
 		releaseVersionString := "Hugo Static Site Generator v0.36.1 darwin/amd64 BuildDate:"
-		version, err := ParseAndCheckVersion([]byte(releaseVersionString))
+		version, err := ParseVersion([]byte(releaseVersionString))
 		require.NoError(t, err, "ParseAndCheckVersion should not fail on a release input version string")
 		require.Equal(t, version, "0.36.1", "ParseAndCheckVersion should return 0.36.1")
 	}
 	{
 		debugVersionString := "Hugo Static Site Generator v0.33-DEV darwin/amd64 BuildDate:"
-		version, err := ParseAndCheckVersion([]byte(debugVersionString))
+		version, err := ParseVersion([]byte(debugVersionString))
 		require.NoError(t, err, "ParseAndCheckVersion should not fail on a debug input version string")
 		require.Equal(t, version, "0.33", "ParseAndCheckVersion should return 0.33")
 	}
+}
+
+func TestCheckMinimumRequiredVersion(t *testing.T) {
+	require.NoError(t, CheckMinimumRequiredVersion("0.33-DEV"), "Hugo version 0.33-DEV should be good enough")
+	require.NoError(t, CheckMinimumRequiredVersion("0.36.1"), "Hugo version 0.36.1 should be good enough")
+	require.Error(t, CheckMinimumRequiredVersion("0.31-DEV"), "Hugo version 0.31-DEV is too old")
+	require.Error(t, CheckMinimumRequiredVersion("0.31.32"), "Hugo version 0.32.32 is too old")
+	require.Error(t, CheckMinimumRequiredVersion("bring thee us a shrubbery"), "this is not a version")
 }
