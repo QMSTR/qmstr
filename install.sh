443d59f7b633dc393aa14a560a898608e37bfcd4
Mirko Boehm (Endocode) <mirko@endocode.com>
2018-03-27 17:28:37 +0200

    Add qmstr to the install script.

--- install.sh
+++ install.sh
@@ -2,10 +2,12 @@
 go get -u github.com/golang/protobuf/protoc-gen-go
 go get -u github.com/golang/dep/cmd/dep
 
-go get github.com/QMSTR/qmstr
+go get -u github.com/QMSTR/qmstr
 (cd $GOPATH/src/github.com/QMSTR/qmstr; dep ensure)
 go generate github.com/QMSTR/qmstr/cmd/qmstr-master
 
 go install github.com/QMSTR/qmstr/cmd/qmstr-master
 go install github.com/QMSTR/qmstr/cmd/qmstr-wrapper
 go install github.com/QMSTR/qmstr/cmd/qmstr-cli
+go install github.com/QMSTR/qmstr/cmd/qmstr
+echo Done.
