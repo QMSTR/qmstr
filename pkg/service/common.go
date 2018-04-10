a336de771baffbaf5d14b1fef6d170ebd12fe03a
Markus Herpich <markus@endocode.com>
2018-04-10 11:49:30 +0200

    Use constants for nodetype
    To not use magic numbers use constants for the nodetypes

--- pkg/service/common.go
+++ pkg/service/common.go
@@ -2,6 +2,15 @@ package service
 
 import "path/filepath"
 
+const (
+	_ = iota
+	NodeTypeFileNode
+	NodeTypeInfoNode
+	NodeTypeDataNode
+	NodeTypeAnalyzerNode
+	NodeTypePackageNode
+)
+
 func NewFileNode(path string, hash string) FileNode {
 	node := FileNode{
 		Path: path,
