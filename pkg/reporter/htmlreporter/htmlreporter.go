8b0c78a5de23dd144bdd0039fd6f09648e1f72cb
Markus Herpich <markus@endocode.com>
2018-04-30 14:17:05 +0200

    html-reporter use configured output directory
    Let the html reporter use the configured output directory to place the
    tarball in.

--- pkg/reporter/htmlreporter/htmlreporter.go
+++ pkg/reporter/htmlreporter/htmlreporter.go
@@ -3,6 +3,7 @@ package htmlreporter
 import (
 	"bufio"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -32,11 +33,18 @@ type HTMLReporter struct {
 	Keep          bool
 	baseURL       string
 	siteData      SiteData
+	packageDir    string
 }
 
 // Configure sets up the working directory for this reporting run.
 // It is part of the ReporterModule interface.
 func (r *HTMLReporter) Configure(config map[string]string) error {
+	if outDir, ok := config["outputDir"]; ok {
+		r.packageDir = outDir
+	} else {
+		return errors.New("no output directory configured")
+	}
+
 	if val, ok := config["keep"]; ok && val == "true" {
 		r.Keep = true
 	}
@@ -102,12 +110,10 @@ func (r *HTMLReporter) PostReport() error {
 	log.Printf("QMSTR reports generated in %v", staticHTMLContentDir)
 
 	// Create the reports package (for publishing, etc).
-	// ... TODO: configure default target directory for every reporter (uses CWD at the moment)
-	packagePath, _ := os.Getwd()
-	if err := CreateReportsPackage(r.workingDir, staticHTMLContentDir, packagePath); err != nil {
-		return fmt.Errorf("error packaging report to %v: %v", packagePath, err)
+	if err := CreateReportsPackage(r.workingDir, staticHTMLContentDir, r.packageDir); err != nil {
+		return fmt.Errorf("error packaging report to %v: %v", r.packageDir, err)
 	}
-	log.Printf("QMSTR reports package generated in %v", packagePath)
+	log.Printf("QMSTR reports package generated in %v", r.packageDir)
 	return nil
 }
 
@@ -285,7 +291,7 @@ func CreateStaticHTML(workingdir string) (string, error) {
 	return outputDir, nil
 }
 
-// CreateReportsPackage creates a tarball of the static HTML reports in the packagePath directory.
+// CreateReporCreateReportsPackagetsPackage creates a tarball of the static HTML reports in the packagePath directory.
 func CreateReportsPackage(workingDir string, contentDir string, packagePath string) error {
 	outputFile := path.Join(packagePath, "qmstr-reports.tar.bz2")
 	cmd := exec.Command("tar", "cfj", outputFile, "-C", workingDir, contentDir)
