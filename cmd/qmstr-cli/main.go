8b81b6129d11a10a51ba70bad8450b48a40bf2ab
Markus Herpich <markus@endocode.com>
2018-05-03 16:05:17 +0200

    Move proto buffer files to generic location
    Since the protobuf files will be used to generate java and python code
    they should not be in a go specific directory.

--- cmd/qmstr-cli/main.go
+++ cmd/qmstr-cli/main.go
@@ -1,4 +1,4 @@
-//go:generate protoc -I ../../pkg/service --go_out=plugins=grpc:../../pkg/service ../../pkg/service/controlservice.proto
+//go:generate protoc -I ../../proto --go_out=plugins=grpc:../../pkg/service ../../proto/datamodel.proto ../../proto/analyzerservice.proto ../../proto/buildservice.proto ../../proto/controlservice.proto  ../../proto/reportservice.proto
 package main
 
 import (
