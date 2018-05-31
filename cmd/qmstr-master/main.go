bf85813e96e39b16b9e1d9c1f8a8500602fdb183
Markus Herpich <markus@endocode.com>
2018-05-31 08:18:37 +0200

    Move config file parsing to master main
    Pull out config parsing in order to adapt it before passing it to the
    server startup.

--- cmd/qmstr-master/main.go
+++ cmd/qmstr-master/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 
+	"github.com/QMSTR/qmstr/pkg/config"
 	"github.com/QMSTR/qmstr/pkg/master"
 	flag "github.com/spf13/pflag"
 )
@@ -11,7 +12,12 @@ func main() {
 	configFile := flag.String("config", "qmstr.yaml", "Set the qmstr configuration file.")
 	flag.Parse()
 
-	masterRun, err := master.InitAndRun(*configFile)
+	masterConfig, err := config.ReadConfigFromFile(*configFile)
+	if err != nil {
+		log.Fatalf("Failed to read configuration %v", err)
+	}
+
+	masterRun, err := master.InitAndRun(&masterConfig)
 	if err != nil {
 		log.Fatalf("Cannot start QMSTR Server: %v\n", err)
 	}
