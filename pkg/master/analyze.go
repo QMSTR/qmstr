bf7f1f9a493910af0af8f040ea58f6fb505aaef8
Giasemi Seisa <giasemi.seisa@endocode.com>
2018-06-05 14:55:52 +0200

    Implement Confidence score
    Confidence score is used as a normalised ranking of info nodes.

--- pkg/master/analyze.go
+++ pkg/master/analyze.go
@@ -131,6 +131,7 @@ func (phase *serverPhaseAnalysis) SendNodes(in *service.AnalysisMessage) (*servi
 			// prevent inserting data nodes twice
 			infoNode.DataNodes = nil
 			infoNode.Analyzer = append(infoNode.Analyzer, phase.currentAnalyzer)
+			infoNode.ConfidenceScore = inode.ConfidenceScore
 
 			inodes.Inodes[idx] = infoNode
 		}
