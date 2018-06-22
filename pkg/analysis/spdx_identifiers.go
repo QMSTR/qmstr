71fa30e35bcb5e144f9a822555135d12e83e438e
Giasemi Seisa <giasemi.seisa@endocode.com>
2018-06-22 17:02:42 +0200

    Check for valid spdx identifier
    Check if spdx analyzer detected a valid spdx license identifier.

---
+++ pkg/analysis/spdx_identifiers.go
@@ -0,0 +1,23 @@
+package analysis
+
+var SpdxLicenses = map[string]struct{}{
+	"0BSD":                             struct{}{},
+	"AGPL-1.0":                         struct{}{},
+	"GPL-1.0-only":                     struct{}{},
+	"GPL-1.0-or-later":                 struct{}{},
+	"GPL-2.0-only":                     struct{}{},
+	"GPL-2.0-or-later":                 struct{}{},
+	"GPL-3.0-only":                     struct{}{},
+	"GPL-3.0-or-later":                 struct{}{},
+	"GPL-1.0+":                         struct{}{},
+	"GPL-1.0":                          struct{}{},
+	"GPL-2.0+":                         struct{}{},
+	"GPL-2.0":                          struct{}{},
+	"GPL-3.0+":                         struct{}{},
+	"GPL-3.0":                          struct{}{},
+	"GPL-2.0-with-autoconf-exception":  struct{}{},
+	"GPL-2.0-with-bison-exception":     struct{}{},
+	"GPL-2.0-with-classpath-exception": struct{}{},
+	"GPL-2.0-with-font-exception":      struct{}{},
+	"GPL-2.0-with-GCC-exception":       struct{}{},
+}
