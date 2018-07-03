c74c685dedde5966ada3ce1d64322cba4649038b
Markus Herpich <markus@endocode.com>
2018-07-03 09:50:45 +0200

    Change QMSTR_ADDRESS to QMSTR_MASTER
    The address of the qmstr-master server is not QMSTR_ADDRESS anymore but
    QMSTR_MASTER

--- java/gradle/src/main/java/org/qmstr/gradle/QmstrPluginExtension.java
+++ java/gradle/src/main/java/org/qmstr/gradle/QmstrPluginExtension.java
@@ -6,7 +6,7 @@ public class QmstrPluginExtension {
     public QmstrPluginExtension() {
         this.qmstrAddress = "localhost:50051";
 
-        String difAddress = System.getenv("QMSTR_ADDRESS");
+        String difAddress = System.getenv("QMSTR_MASTER");
         if (difAddress != null && !difAddress.trim().isEmpty()){
             this.qmstrAddress = difAddress;
         }
