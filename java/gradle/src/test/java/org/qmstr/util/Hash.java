338088b159882505ddf6bcb20ce577daf9ac8a6b
Markus Herpich <markus@endocode.com>
2018-07-12 11:47:07 +0200

    Fix checksum test
    The hashing method changed from sha-256 to sha-1.

--- java/gradle/src/test/java/org/qmstr/util/Hash.java
+++ java/gradle/src/test/java/org/qmstr/util/Hash.java
@@ -12,7 +12,7 @@ class HashTests{
     @Test
     void checksumTest() {
             File hashtest = new File(getClass().getResource("/hashtest").getFile());
-            assertEquals("e72b4044adf37a8a925ee6b046fe2ee3146ede625f9f89636957a35e10be83aa", Hash.getChecksum(hashtest));
+            assertEquals("45e51db6f37b0b8af21c7822ed4b470e6565f931", Hash.getChecksum(hashtest));
     }
 
 }
