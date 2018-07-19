53eb80f9d6c853772e09dd89262438d49a1f4dca
Mirko Boehm (Endocode) <mirko@endocode.com>
2018-07-19 19:12:29 +0200

    Make /usr/local writable to the test user.

--- ci/vagrant-test-documentation-driver.sh
+++ ci/vagrant-test-documentation-driver.sh
@@ -2,10 +2,13 @@
 #
 # WARNING: Only call this to be executed in an automated CI
 # setup. This script will modify the home directory of the current
-# user!
+# user and the host file system!
 #
-# This script is to be called from the repository root.
+# This script is to be called from the repository root. It also
+# assumes that /vagrant points to a working copy of the QMSTR/qmstr
+# repository.
 set -e
+sudo chown -R $USER:$USER /usr/local/
 export GOPATH=$HOME/test_go
 export PATH=$GOPATH/bin:$PATH
 BASEDIR=$GOPATH/github/QMSTR
