45db6f72a9e7fc4ed5ae0249af774bcb5c637959
Mirko Boehm (Endocode) <mirko@endocode.com>
2018-07-19 19:51:56 +0200

    Add Docker to the VM setup.
    With this, the current shelldoc tests all pass.

--- ci/vagrant-bootstrap.sh
+++ ci/vagrant-bootstrap.sh
@@ -7,6 +7,10 @@ echo "Installing protobuf compiler..."
 apt-get -yqq install protobuf-compiler
 echo "Installing Go protobuf interface..."
 apt-get -yqq install golang-goprotobuf-dev
+echo "Installing Docker..."
+apt-get -yqq install docker.io
+echo "Adding vagrant user to the docker group..."
+addgroup vagrant docker
 echo "Installing shelldoc..."
 go get -u github.com/endocode/shelldoc/cmd/shelldoc && mv -f ~/go/bin/shelldoc /usr/local/bin/
 echo "Done."
