11653f688691a5306d64d8157be349354a26f3eb
Markus Herpich <markus@endocode.com>
2018-08-15 15:50:32 +0200

    Add test for empty BuildConfig

--- pkg/config/config_test.go
+++ pkg/config/config_test.go
@@ -299,6 +299,51 @@ package:
 	}
 }
 
+func TestEmptyBuildConfig(t *testing.T) {
+
+	var config = `
+package:
+  name: "The Test"
+  buildconfig: ""
+  metadata:
+    Vendor: "Endocode"
+    OcFossLiaison: "Mirko Boehm"
+    OcComplianceContact: "foss@endocode.com"
+  server:
+    rpcaddress: ":12345"
+    dbaddress: "testhost:54321"
+    dbworkers: 4
+  analysis:
+    - analyzer: test-analyzer
+      name: "The Testalyzer"
+      selector: sourcecode
+      pathsub:
+        - old: "/the/path"
+          new: "/buildroot"
+      config:
+        workdir: "/buildroot"
+    - analyzer: test-analyzer-2
+      name: "The Testalyzer 2"
+      selector: sourcecode
+      pathsub:
+        - old: "/the/path"
+          new: "/buildroot"
+      config:
+        workdir: "/buildroot"
+        testfile: "/the/test"
+  reporting:
+    - reporter: test-reporter
+      name: "The test reporter"
+      config:
+        tester: "Endocode"
+`
+	_, err := ReadConfigFromBytes([]byte(config))
+	if err == nil || err.Error() != "Invalid empty BuildConfig" {
+		t.Logf("Broken config %v", err)
+		t.Fail()
+	}
+}
+
 func Test_posixFullyPortableFilename(t *testing.T) {
 	type args struct {
 		filename string
