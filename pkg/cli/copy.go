0a7cd2ea4b47511374b4f622d14c5a003c8292d1
Markus Herpich <markus@endocode.com>
2018-08-23 12:21:50 +0200

    Clean result directory before copying
    In order to produce only valid output clean the result directory before
    copying results.
    
    refs #115

--- pkg/cli/copy.go
+++ pkg/cli/copy.go
@@ -1,7 +1,7 @@
 package cli
 
 import (
-	"log"
+	"fmt"
 	"os"
 	"path/filepath"
 
@@ -19,7 +19,9 @@ var copyCmd = &cobra.Command{
 	Short: "Copy results from qmstr",
 	Long:  `Copy report results from qmstr server.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		copyResults()
+		if err := copyResults(); err != nil {
+			Log.Fatalf("copying results failed: %v", err)
+		}
 	},
 }
 
@@ -27,25 +29,25 @@ func init() {
 	rootCmd.AddCommand(copyCmd)
 }
 
-func copyResults() {
+func copyResults() error {
 	ctx := context.Background()
 	cli, err := client.NewEnvClient()
 	if err != nil {
-		Log.Fatalf("Failed to create docker client %v", err)
+		return fmt.Errorf("failed to create docker client %v", err)
 	}
 	mID, _, err := docker.GetMasterInfo(ctx, cli)
 	if err != nil {
-		log.Fatal(err)
+		return fmt.Errorf("failed to obtain qmstr-master info %v", err)
 	}
 
 	configdata, err := docker.GetMasterConfig(ctx, cli, mID, internalConfigPath)
 	if err != nil {
-		Log.Fatalf("Can not load master configuration from container : %v", err)
+		return fmt.Errorf("can not load master configuration from container: %v", err)
 	}
 	Debug.Printf("Got config from master: %s", configdata)
 	config, err := config.ReadConfigFromBytes(configdata)
 	if err != nil {
-		Log.Fatalf("Can not read master configuration from container : %v", err)
+		return fmt.Errorf("can not read master configuration from container: %v", err)
 	}
 
 	outdir := config.Server.OutputDir
@@ -53,11 +55,16 @@ func copyResults() {
 		var err error
 		outdir, err = os.Getwd()
 		if err != nil {
-			Log.Fatalf("unable to determine current working directory")
+			return fmt.Errorf("unable to determine current working directory: %v", err)
 		}
 		outdir = filepath.Join(outdir, "qmstr")
 	}
 
+	// clean outdir if it exists
+	if err = os.RemoveAll(outdir); err != nil {
+		return fmt.Errorf("failed to clean result directory: %v", err)
+	}
+
 	Debug.Printf("writing results to %s", outdir)
-	docker.CopyResults(ctx, cli, mID, outdir)
+	return docker.CopyResults(ctx, cli, mID, outdir)
 }
