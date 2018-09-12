f8a38db8f364494481b983c4a46df2a4edefbccc
Markus Herpich <markus@endocode.com>
2018-09-12 14:32:29 +0200

    Introduce wrapper chaining
    In order to call not the actual compiler but the next wrapper e.g.
    ccache that wrapper must be called directly to avoid a loop where the
    next wrapper calls qmstr-wrapper again and so on.

--- pkg/wrapper/fileutil.go
+++ pkg/wrapper/fileutil.go
@@ -8,23 +8,54 @@ import (
 	"github.com/QMSTR/qmstr/pkg/common"
 )
 
-// FindActualProgram discovers the actual program that is wrapper on the PATH
+// FindActualProgram discovers the actual program that is wrapped on the PATH
 func FindActualProgram(prog string) (string, error) {
+	me, err := os.Executable()
+	if err != nil {
+		return "", fmt.Errorf("could not find initial executable that started this process: %v", err)
+	}
+	meAbs, err := filepath.Abs(me)
+	if err != nil {
+		return "", fmt.Errorf("failed finding absolute path: %v", err)
+	}
+
+	paths := FindExecutablesOnPath(prog)
+	countProgs := len(paths)
+	for i, path := range paths {
+		pathAbs, err := filepath.Abs(path)
+		if err != nil {
+			continue
+		}
+
+		pathtocheck, err := filepath.EvalSymlinks(pathAbs)
+		if err != nil {
+			return "", fmt.Errorf("failed evaluation: %v", err)
+		}
+
+		if filepath.Clean(meAbs) == pathtocheck {
+			// +1 next program after me is the target
+			if countProgs <= i+1 {
+				break
+			}
+			return paths[i+1], nil
+		}
+	}
+	return "", fmt.Errorf("executable file %s not found in %v", prog, paths)
+}
+
+// FindExecutablesOnPath finds and returns all reachable executables for the given progname
+func FindExecutablesOnPath(progname string) []string {
+	var paths []string
 	path := os.Getenv("PATH")
-	foundWrapper := false
 	for _, dir := range filepath.SplitList(path) {
 		if dir == "" {
 			// Unix shell semantics: path element "" means "."
 			dir = "."
 		}
-		path := filepath.Join(dir, prog)
+		path := filepath.Join(dir, progname)
 		if err := common.CheckExecutable(path); err == nil {
-			if foundWrapper {
-				return path, nil
-			}
-			// First hit is the wrapper
-			foundWrapper = true
+			paths = append(paths, path)
 		}
 	}
-	return "", fmt.Errorf("executable file %s not found in [%s]", prog, path)
+	return paths
 }
