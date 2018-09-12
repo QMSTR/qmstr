ef9214bde6ccb78b0cbc0ded263129b9816251ed
Markus Herpich <markus@endocode.com>
2018-09-12 13:47:04 +0200

    Pass through ccache
    Mount ccache in client container and set CCACHE_DIR accordingly.

--- pkg/common/constants.go
+++ pkg/common/constants.go
@@ -6,7 +6,11 @@ const (
 	QMSTRADDRENV = "QMSTR_MASTER"
 	// QMSTRDEBUGENV is the name of the environment variable that defines if qmstr runs in debug mode
 	QMSTRDEBUGENV = "QMSTR_DEBUG"
+	CCACHEDIRENV  = "CCACHE_DIR"
 )
 
 // ContainerBuildDir is where the source code gets mounted in the qmstr-master container
 const ContainerBuildDir = "/buildroot"
+
+//
+const ContainerCcacheDir = "/ccache"
