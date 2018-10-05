dff85b91b9fade64bb5ae8af1db24d5fa46d6514
Giasemi Seisa <giasemi.seisa@endocode.com>
2018-10-05 17:32:05 +0200

    Wrap objcopy command

--- cmd/qmstr/qmstr.go
+++ cmd/qmstr/qmstr.go
@@ -38,7 +38,7 @@ var (
 	Debug *golog.Logger
 	// Log is the standard logger
 	Log         *golog.Logger
-	wrappedCmds = []string{"gcc", "ar", "ld", "as"}
+	wrappedCmds = []string{"gcc", "ar", "ld", "as", "objcopy"}
 )
 
 func main() {
