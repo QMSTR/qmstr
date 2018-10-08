4fe9fbf8dd53aa80f2c3197ad934dcde0c9edc2b
Giasemi Seisa <giasemi.seisa@endocode.com>
2018-10-08 11:28:09 +0200

    Add tests for the as assembler

---
+++ pkg/gnubuilder/asbuilder/asbuilder_test.go
@@ -0,0 +1,38 @@
+package asbuilder_test
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"testing"
+
+	builder "github.com/QMSTR/qmstr/pkg/gnubuilder/asbuilder"
+)
+
+func getTestCompiler() *builder.AsBuilder {
+	return builder.NewAsBuilder("/tmp", log.New(os.Stdout, "TESTING", log.LstdFlags), false)
+}
+
+func TestOutputFlag(t *testing.T) {
+	as := getTestCompiler()
+	as.Analyze([]string{"as", "a.s", "-o", "a.o"})
+	if as.Input != "a.s" && as.Output != "a.o" {
+		t.Fail()
+	}
+}
+
+func TestNoOutputFlag(t *testing.T) {
+	as := getTestCompiler()
+	as.Analyze([]string{"as", "a.s"})
+	if as.Input != "a.s" && as.Output != "a.o" {
+		t.Fail()
+	}
+}
+
+func TestCleanCmdBoolArgs(t *testing.T) {
+	as := getTestCompiler()
+	as.Analyze([]string{"as", "--32", "a.s", "-o", "a.o"})
+	if fmt.Sprintf("%v", as.Args) != "[a.s -o a.o]" {
+		t.Fail()
+	}
+}
