385f6cbfa113a3b9e8d729ebcfd0c6a42392bad8
Giasemi Seisa <giasemi.seisa@endocode.com>
2018-10-11 17:56:04 +0200

    Create Hash in non build phase
    Create Hashes when we are adding file nodes in other phases apart from
    the build phase.

--- pkg/builder/builder.go
+++ pkg/builder/builder.go
@@ -1,16 +1,13 @@
 package builder
 
 import (
-	"crypto/sha1"
-	"fmt"
-	"io"
 	"log"
-	"os"
 	"path/filepath"
 
 	"github.com/pkg/errors"
 	"github.com/spf13/afero"
 
+	"github.com/QMSTR/qmstr/pkg/common"
 	"github.com/QMSTR/qmstr/pkg/qmstr/service"
 )
 
@@ -37,40 +34,13 @@ func NewGeneralBuilder(logger *log.Logger, debug bool) GeneralBuilder {
 
 func NewFileNode(path string, fileType string) *service.FileNode {
 	filename := filepath.Base(path)
-	hash, err := hash(path)
+	hash, err := common.Hash(path)
 	broken := false
 	if err != nil {
 		hash = "nohash" + path
 		broken = true
 	}
 	return &service.FileNode{Name: filename, Type: fileType, Path: path, Hash: hash, Broken: broken}
-}
-
-func hash(fileName string) (string, error) {
-	h := sha1.New()
-	f, err := os.Open(fileName)
-	if err != nil {
-		return "", err
-	}
-	buf := make([]byte, 0, 4*1024)
-	for {
-		n, err := f.Read(buf[:cap(buf)])
-		buf = buf[:n]
-		if n == 0 {
-			if err == nil {
-				continue
-			}
-			if err == io.EOF {
-				break
-			}
-			return "", err
-		}
-		h.Write(buf)
-		if err != nil && err != io.EOF {
-			return "", err
-		}
-	}
-	return fmt.Sprintf("%x", h.Sum(nil)), nil
 }
 
 func CleanCmd(commandline []string, cleanIdx []int, debug bool, logger *log.Logger) []string {
