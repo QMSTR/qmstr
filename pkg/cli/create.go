8e0a4a3648974753f88d014f3fdf4d685a195693
Markus Herpich <markus@endocode.com>
2019-03-01 20:51:54 +0100

    Add flag generation
    To maintain flags for the different node types flags are generated from
    the allowed fields of the different node types.

--- pkg/cli/create.go
+++ pkg/cli/create.go
@@ -1,37 +1,82 @@
 package cli
 
 import (
+	"errors"
 	"log"
 	"reflect"
 
 	"github.com/QMSTR/qmstr/pkg/service"
 	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
+	"golang.org/x/net/context"
 )
+
+var currentNode interface{}
+var cmdFlags *pflag.FlagSet
 
 var createCmd = &cobra.Command{
 	Use:   "create",
 	Short: "create a new node",
 	Long:  "create a new node described by an node identifier",
 	Run: func(cmd *cobra.Command, args []string) {
-		setUpControlService()
-		createNode(args)
+		setUpBuildService()
+		cmdFlags = cmd.Flags()
+		err := createNode(args[0], true)
+		if err != nil {
+			log.Fatalf("Failed to create node: %v", err)
+		}
 		tearDownServer()
+
 	},
 }
 
 func init() {
+	var err error
+	generatedFlags := &pflag.FlagSet{}
 	rootCmd.AddCommand(createCmd)
+	err = generateFlags(&service.FileNode{}, generatedFlags)
+	if err != nil {
+		log.Fatalf("%v", err)
+	}
+	createCmd.Flags().AddFlagSet(generatedFlags)
+	err = generateFlags(&service.PackageNode{}, generatedFlags)
+	if err != nil {
+		log.Fatalf("%v", err)
+	}
+	createCmd.Flags().AddFlagSet(generatedFlags)
 }
 
-func createNode(args []string) {
-	for _, nodeident := range args {
-		node, err := ParseNodeID(nodeident)
-		if err != nil {
-			log.Fatalf("%v", err)
+func createNode(nodeIdent string, send bool) error {
+	var err error
+	currentNode, err = ParseNodeID(nodeIdent)
+	if err != nil {
+		return err
+	}
+	switch reflect.TypeOf(currentNode) {
+	case reflect.TypeOf((*service.FileNode)(nil)):
+		cmdFlags.Visit(visitFileNodeFlag)
+		log.Printf("Got file node %v", currentNode.(*service.FileNode).Describe(true, ""))
+		if send {
+			stream, err := buildServiceClient.Build(context.Background())
+			if err != nil {
+				return err
+			}
+			stream.Send(currentNode.(*service.FileNode))
+			br, err := stream.CloseAndRecv()
+			if err != nil {
+				return err
+			}
+			if !br.Success {
+				return errors.New("sending filenode failed")
+			}
+			return nil
 		}
-		switch reflect.TypeOf(node) {
-		case reflect.TypeOf((*service.FileNode)(nil)):
-			log.Printf("Got node %v", node.(*service.FileNode))
+	case reflect.TypeOf((*service.PackageNode)(nil)):
+		cmdFlags.Visit(visitPkgNodeFlag)
+		log.Printf("Got pkg node %v", currentNode.(*service.PackageNode).Describe(true))
+		if send {
+			log.Fatalf("Sending package nodes not yet supported")
 		}
 	}
+	return nil
 }
---
+++ pkg/cli/create_test.go
@@ -0,0 +1,69 @@
+package cli
+
+import (
+	"log"
+	"testing"
+
+	"github.com/QMSTR/qmstr/pkg/service"
+	"github.com/spf13/pflag"
+)
+
+func TestStringFlagNodeCreation(t *testing.T) {
+	cmdFlags = &pflag.FlagSet{}
+	err := generateFlags(&service.FileNode{}, cmdFlags)
+	if err != nil {
+		log.Printf("%v", err)
+	}
+	cmdFlags.Parse([]string{"--FileNodeName", "foobar", "--FileNodePath", "/dev/null"})
+	if err = createNode("file:hash:12345", false); err != nil {
+		t.FailNow()
+	}
+	if currentNode.(*service.FileNode).Hash != "12345" || currentNode.(*service.FileNode).Name != "foobar" || currentNode.(*service.FileNode).Path != "/dev/null" {
+		t.Fail()
+	}
+}
+
+func TestBoolFlagNodeCreation(t *testing.T) {
+	cmdFlags = &pflag.FlagSet{}
+	err := generateFlags(&service.FileNode{}, cmdFlags)
+	if err != nil {
+		log.Printf("%v", err)
+	}
+	cmdFlags.Parse([]string{"--FileNodeBroken"})
+	if err = createNode("file:hash:12345", false); err != nil {
+		t.FailNow()
+	}
+	if currentNode.(*service.FileNode).Hash != "12345" || !currentNode.(*service.FileNode).Broken {
+		t.Fail()
+	}
+}
+
+func TestIntFlagNodeCreation(t *testing.T) {
+	cmdFlags = &pflag.FlagSet{}
+	err := generateFlags(&service.FileNode{}, cmdFlags)
+	if err != nil {
+		log.Printf("%v", err)
+	}
+	cmdFlags.Parse([]string{"--FileNodeFileType", "2"})
+	if err = createNode("file:hash:12345", false); err != nil {
+		t.FailNow()
+	}
+	if currentNode.(*service.FileNode).Hash != "12345" || currentNode.(*service.FileNode).FileType != service.FileNode_INTERMEDIATE {
+		t.Fail()
+	}
+}
+
+func TestStringPackageNodeCreation(t *testing.T) {
+	cmdFlags = &pflag.FlagSet{}
+	err := generateFlags(&service.PackageNode{}, cmdFlags)
+	if err != nil {
+		log.Printf("%v", err)
+	}
+	cmdFlags.Parse([]string{"--PackageNodeBuildConfig", "foobar"})
+	if err = createNode("package:TestPackage", false); err != nil {
+		t.FailNow()
+	}
+	if currentNode.(*service.PackageNode).Name != "TestPackage" || currentNode.(*service.PackageNode).BuildConfig != "foobar" {
+		t.Fail()
+	}
+}
