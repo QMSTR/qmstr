53a771acad584d91889a9c654fb2ea1fde0ddd46
Markus Herpich <markus@endocode.com>
2019-03-01 21:43:33 +0100

    Remove obsolet generate line

--- cmd/qmstrctl/main.go
+++ cmd/qmstrctl/main.go
@@ -1,4 +1,3 @@
-//go:generate protoc -I ../../proto --go_out=plugins=grpc:../../pkg/service ../../proto/datamodel.proto ../../proto/analyzerservice.proto ../../proto/buildservice.proto ../../proto/controlservice.proto  ../../proto/reportservice.proto
 package main
 
 import (
