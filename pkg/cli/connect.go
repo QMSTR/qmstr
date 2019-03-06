55d5561b2127603b48713ccd2745eb4e68a348ba
Nikolas Sepos <nikolas@endocode.com>
2019-03-06 16:47:05 +0100

    Update Connect
    * Support connecting fileNodes to packageNode
    * Relevant master method implemented

--- pkg/cli/connect.go
+++ pkg/cli/connect.go
@@ -14,21 +14,28 @@ var validFileToFileEdges = []string{
 	"dependencies",
 }
 
+var validFileToPackageEdges = []string{
+	"targets",
+}
+
 var connectCmdFlags = struct {
-	fileToFileEdge string
+	fileToFileEdge    string
+	fileToPackageEdge string
 }{}
 
 func init() {
 	rootCmd.AddCommand(connectCmd)
 	connectCmd.Flags().StringVar(&connectCmdFlags.fileToFileEdge, "fileToFileEdge",
 		"derivedFrom", fmt.Sprintf("Edge to use when connecting FileNode to FileNode. One of %v", validFileToFileEdges))
+	connectCmd.Flags().StringVar(&connectCmdFlags.fileToPackageEdge, "fileToPackageEdge",
+		"targets", fmt.Sprintf("Edge to use when connecting FileNode to PackageNode. One of %v", validFileToPackageEdges))
 }
 
 var connectCmd = &cobra.Command{
 	Use:   "connect",
 	Short: "Connect nodes with specific edges",
-	Long: `Usage: qmstrctl connect <this> <that>
-Connect Node <this> to Node <that>. In case of multiple edges for the specified types you can use --<type>To<type>Edge flag to specify the edge you want.`,
+	Long: `Usage: qmstrctl connect <that> <this>...
+Connect to Node <that> Node(s) <this>. In case of multiple edges for the specified types you can use --<type>To<type>Edge flag to specify the edge you want.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		setUpControlService()
 		setUpBuildService()
@@ -37,15 +44,11 @@ Connect Node <this> to Node <that>. In case of multiple edges for the specified 
 		}
 		tearDownServer()
 	},
-	Args: cobra.ExactArgs(2),
+	Args: cobra.MinimumNArgs(2),
 }
 
 func connectCmdRun(cmd *cobra.Command, args []string) error {
-	thisID, err := ParseNodeID(args[0])
-	if err != nil {
-		return fmt.Errorf("ParseNodeID fail for %q: %v", args[0], err)
-	}
-	thatID, err := ParseNodeID(args[1])
+	thatID, err := ParseNodeID(args[0])
 	if err != nil {
 		return fmt.Errorf("ParseNodeID fail for %q: %v", args[1], err)
 	}
@@ -55,6 +58,31 @@ func connectCmdRun(cmd *cobra.Command, args []string) error {
 		that, err := getUniqueFileNode(thatVal)
 		if err != nil {
 			return fmt.Errorf("get unique \"that\" node fail: %v", err)
+		}
+		err = connectToFileNode(that, args[1:])
+		if err != nil {
+			return fmt.Errorf("connectToFileNode fail: %v", err)
+		}
+	case *service.PackageNode:
+		that, err := controlServiceClient.GetPackageNode(context.Background(), &service.PackageRequest{})
+		if err != nil {
+			return fmt.Errorf("get package node fail: %v", err)
+		}
+		err = connectToPackageNode(that, args[1:])
+		if err != nil {
+			return fmt.Errorf("connectToPackageNode fail: %v", err)
+		}
+	default:
+		return fmt.Errorf("unsuported node type %T", thatVal)
+	}
+	return nil
+}
+
+func connectToFileNode(node *service.FileNode, args []string) error {
+	for _, nID := range args {
+		thisID, err := ParseNodeID(nID)
+		if err != nil {
+			return fmt.Errorf("ParseNodeID fail for %q: %v", args[0], err)
 		}
 		switch thisVal := thisID.(type) {
 		// FileNode -> FileNode
@@ -66,22 +94,61 @@ func connectCmdRun(cmd *cobra.Command, args []string) error {
 			// Which edge
 			switch connectCmdFlags.fileToFileEdge {
 			case "derivedFrom":
-				that.DerivedFrom = append(that.DerivedFrom, this)
+				node.DerivedFrom = append(node.DerivedFrom, this)
 			case "dependencies":
-				that.Dependencies = append(that.Dependencies, this)
+				node.Dependencies = append(node.Dependencies, this)
 			default:
 				return fmt.Errorf("unknown edge for FileNode -> FileNode: %s", connectCmdFlags.fileToFileEdge)
 			}
 		default:
 			return fmt.Errorf("cannot connect %T to FileNode", thisVal)
 		}
-		// ship it back
-		err = sendFileNode(that)
+	}
+	// ship it back
+	err := sendFileNode(node)
+	if err != nil {
+		return fmt.Errorf("sending FileNode fail: %v", err)
+	}
+	return nil
+}
+
+func connectToPackageNode(node *service.PackageNode, args []string) error {
+	stream, err := buildServiceClient.Package(context.Background())
+	if err != nil {
+		return err
+	}
+	for _, nID := range args {
+		thisID, err := ParseNodeID(nID)
 		if err != nil {
-			return fmt.Errorf("sending FileNode fail: %v", err)
+			return fmt.Errorf("ParseNodeID fail for %q: %v", args[0], err)
 		}
-	default:
-		return fmt.Errorf("unsuported node type %T", thatVal)
+		switch thisVal := thisID.(type) {
+		// FileNode -> FileNode
+		case *service.FileNode:
+			this, err := getUniqueFileNode(thisVal)
+			if err != nil {
+				return fmt.Errorf("get unique \"this\" node fail: %v", err)
+			}
+			// Which edge
+			switch connectCmdFlags.fileToPackageEdge {
+			case "targets":
+				err = stream.Send(this)
+				if err != nil {
+					return fmt.Errorf("send fileNode to pkg stream fail: %v", err)
+				}
+			default:
+				return fmt.Errorf("unknown edge for FileNode -> FileNode: %s", connectCmdFlags.fileToPackageEdge)
+			}
+		default:
+			return fmt.Errorf("cannot connect %T to FileNode", thisVal)
+		}
+	}
+	res, err := stream.CloseAndRecv()
+	if err != nil {
+		return fmt.Errorf("close stream fail: %v", err)
+	}
+	if !res.Success {
+		return fmt.Errorf("sending node fail: %v", err)
 	}
 	return nil
 }
--- pkg/master/master.go
+++ pkg/master/master.go
@@ -5,14 +5,12 @@ import (
 	"bytes"
 	"errors"
 	"fmt"
+	"io"
 	"log"
 	"net"
 	"strings"
 	"sync"
 	"sync/atomic"
-
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
 
 	"golang.org/x/net/context"
 
@@ -88,7 +86,26 @@ func (s *server) GetBOM(ctx context.Context, in *service.BOMRequest) (*service.B
 }
 
 func (s *server) Package(stream service.BuildService_PackageServer) error {
-	return status.Error(codes.Unimplemented, "Package method unimplemented")
+	db, err := s.currentPhase.getDataBase()
+	if err != nil {
+		return err
+	}
+	pkg, err := db.GetPackageNode()
+	if err != nil {
+		return err
+	}
+	for {
+		fl, err := stream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return fmt.Errorf("recv fileNode fail: %v", err)
+		}
+		pkg.Targets = append(pkg.Targets, fl)
+	}
+	db.AddPackageNode(pkg)
+	return stream.SendAndClose(&service.BuildResponse{Success: true})
 }
 
 func (s *server) GetPackageNode(ctx context.Context, in *service.PackageRequest) (*service.PackageNode, error) {
