88852d0c6c88347a095237c3c5d0b94a22db5e11
GiasemiSh <giasemi.seisa@endocode.com>
2019-04-08 18:53:18 +0200

    Describe a project node
    Extend describe command to print the description of a project node.

--- pkg/cli/describe.go
+++ pkg/cli/describe.go
@@ -28,6 +28,7 @@ attribute can be:
 and value, the value of the attribute.
 	`,
 	Run: func(cmd *cobra.Command, args []string) {
+		setUpBuildService()
 		setUpControlService()
 		if err := describeNode(args); err != nil {
 			Log.Fatalf("Describe failed: %v", err)
@@ -45,20 +46,26 @@ func describeNode(args []string) error {
 	if len(args) < 1 {
 		return fmt.Errorf("Please provide the node you want to get description for: [type_of_node:attribute:value]")
 	}
-	node, err := ParseNodeID(args[0])
+	n, err := ParseNodeID(args[0])
 	if err != nil {
 		return err
 	}
 
-	switch node.(type) {
+	switch node := n.(type) {
+	case *service.ProjectNode:
+		pNode, err := buildServiceClient.GetProjectNode(context.Background(), &service.ProjectNode{})
+		if err != nil {
+			return err
+		}
+		fmt.Println(pNode.Describe(less))
 	case *service.PackageNode:
 		pkgNode, err := controlServiceClient.GetPackageNode(context.Background(), &service.PackageNode{})
 		if err != nil {
 			return err
 		}
-		fmt.Println(pkgNode.Describe(less))
+		fmt.Println(pkgNode.Describe(less, ""))
 	case *service.FileNode:
-		fNodes, err := controlServiceClient.GetFileNode(context.Background(), &service.GetFileNodeMessage{FileNode: node.(*service.FileNode)})
+		fNodes, err := controlServiceClient.GetFileNode(context.Background(), &service.GetFileNodeMessage{FileNode: node})
 		if err != nil {
 			return err
 		}
