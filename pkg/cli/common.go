d9a2d952044591cd332f17cc957d0b8e45826110
GiasemiSh <giasemi.seisa@endocode.com>
2019-04-09 18:08:50 +0200

    Clean connect cmd
    Move some functionality to make the code more clear and
    understandable.

--- pkg/cli/common.go
+++ pkg/cli/common.go
@@ -1,6 +1,8 @@
 package cli
 
 import (
+	"fmt"
+
 	"github.com/QMSTR/qmstr/pkg/service"
 	"golang.org/x/net/context"
 )
@@ -15,4 +17,23 @@ func getUniqueFileNode(fnode *service.FileNode) (*service.FileNode, error) {
 		return nil, err
 	}
 	return fileNode, nil
+}
+
+func sendFileNode(node *service.FileNode) error {
+	stream, err := buildServiceClient.Build(context.Background())
+	if err != nil {
+		return fmt.Errorf("getting stream for build service fail: %v", err)
+	}
+	err = stream.Send(node)
+	if err != nil {
+		return fmt.Errorf("sending node fail: %v", err)
+	}
+	res, err := stream.CloseAndRecv()
+	if err != nil {
+		return fmt.Errorf("close stream fail: %v", err)
+	}
+	if !res.Success {
+		return fmt.Errorf("sending node fail: %v", err)
+	}
+	return nil
 }
