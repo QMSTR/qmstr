07fef81b5c5ad117ee3a01a950059986f8279346
Mirko Boehm (Endocode) <mirko@endocode.com>
2019-04-26 14:53:17 +0200

    Port reference generation of qmstrctl to the joint implementation.
    References #286.

--- lib/go-qmstr/cli/dev-generate-reference.go
+++ lib/go-qmstr/cli/dev-generate-reference.go
@@ -1,55 +1,10 @@
 package cli
 
 import (
-	"fmt"
-	"log"
-	"os"
-	"path"
-	"path/filepath"
-	"strings"
-	"time"
-
-	"github.com/spf13/cobra"
-	"github.com/spf13/cobra/doc"
+	"github.com/QMSTR/qmstr/lib/go-qmstr/common"
 )
 
-// This command generates the complete command line reference for qmstrctl using the built-in functionality of Cobra:
-var devGenerateReferenceCmd = &cobra.Command{
-	Use:   "dev-generate-reference [path]",
-	Short: "Generate command line reference in the specified path.",
-	Long:  `Generate the command line reference in Markdown format. The pages will contain metadata to integrate into a Hugo web site.`,
-	Args:  cobra.ExactArgs(1),
-	Run: func(cmd *cobra.Command, args []string) {
-		print("Generating command line reference...\n")
-		outputPath := args[0]
-		if err := os.MkdirAll(outputPath, os.ModePerm); err != nil {
-			log.Fatal(err)
-		}
-
-		const fmTemplate = `---
-date: %s
-title: "%s"
-slug: "%s"
----
-`
-
-		filePrepender := func(filename string) string {
-			now := time.Now().Format(time.RFC3339)
-			name := filepath.Base(filename)
-			base := strings.TrimSuffix(name, path.Ext(name))
-			return fmt.Sprintf(fmTemplate, now, strings.Replace(base, "_", " ", -1), base)
-		}
-
-		linkHandler := func(name string) string {
-			base := strings.TrimSuffix(name, path.Ext(name))
-			return "../" + strings.ToLower(base) + "/"
-		}
-		if err := doc.GenMarkdownTreeCustom(rootCmd, outputPath, filePrepender, linkHandler); err != nil {
-			log.Fatal(err)
-		}
-	},
-}
-
 func init() {
-	rootCmd.AddCommand(devGenerateReferenceCmd)
+	cmd := common.CreateGenerateReferenceCmd(rootCmd)
+	rootCmd.AddCommand(cmd)
 }
