f2689ce995c87a23bc1ee10dfcacd7eb8ee8289f
Markus Herpich <markus@endocode.com>
2019-05-03 08:16:22 +0200

    Show authors and licenses in html report
    Render authors and licenses in the html report.

--- lib/go-qmstr/reporting/package_data.go
+++ lib/go-qmstr/reporting/package_data.go
@@ -1,6 +1,7 @@
 package reporting
 
 import (
+	"encoding/json"
 	"github.com/QMSTR/qmstr/lib/go-qmstr/service"
 )
 
@@ -9,35 +10,81 @@ type PackageData struct {
 	Name            string
 	Version         string // Usually a Git hash, but any string can be used
 	LicenseDeclared string
-	Targets         []*service.FileNode
-	Authors         []string
+	Targets         []*Target
 	Project         string // The project this package is associated with.
 }
 
-func GetPackageData(pkg *service.PackageNode, projData *ProjectData) *PackageData {
+type Target struct {
+	Target   *service.FileNode
+	Licenses []string
+	Authors  []string
+}
+
+func GetPackageData(pkg *service.PackageNode, projectName string) *PackageData {
 	packageData := PackageData{
 		Name:            pkg.GetName(),
 		Version:         pkg.GetVersion(),
 		LicenseDeclared: "NO License declared",
-		Project:         projData.Name,
+		Project:         projectName,
 	}
-	authors := []string{}
-	targets := []*service.FileNode{}
+
+	if packageData.Version == "" {
+		packageData.Version = "default"
+	}
+
+	targets := []*Target{}
 	for _, fileNode := range pkg.GetTargets() {
-		targets = append(targets, fileNode)
-		for _, info := range fileNode.AdditionalInfo {
-			if info.Type == "copyright" {
-				for _, d := range info.DataNodes {
-					switch d.Type {
-					case "author":
-						authors = append(authors, d.Data)
-					}
-				}
-			}
-		}
+		// reduce returned data
+		fileNode.DerivedFrom = []*service.FileNode{}
+		fileNode.Dependencies = []*service.FileNode{}
+		targets = append(targets, &Target{Target: fileNode})
 	}
-	packageData.Authors = authors
 	packageData.Targets = targets
 
 	return &packageData
+}
+
+func (p *PackageData) GetAuthors() []string {
+	authors := map[string]struct{}{}
+	for _, target := range p.Targets {
+		for _, author := range target.Authors {
+			authors[author] = struct{}{}
+		}
+	}
+
+	uniqAuthors := []string{}
+
+	for author := range authors {
+		uniqAuthors = append(uniqAuthors, author)
+	}
+	return uniqAuthors
+}
+
+func (p *PackageData) GetLicenses() []string {
+	licenses := map[string]struct{}{}
+	for _, target := range p.Targets {
+		for _, license := range target.Licenses {
+			licenses[license] = struct{}{}
+		}
+	}
+
+	uniqLicenses := []string{}
+
+	for license := range licenses {
+		uniqLicenses = append(uniqLicenses, license)
+	}
+	return uniqLicenses
+}
+
+func (p *PackageData) MarshalJSON() ([]byte, error) {
+	type Alias PackageData
+	return json.Marshal(&struct {
+		Authors  []string
+		Licenses []string
+		*Alias
+	}{
+		Authors:  p.GetAuthors(),
+		Licenses: p.GetLicenses(),
+		Alias:    (*Alias)(p),
+	})
 }
