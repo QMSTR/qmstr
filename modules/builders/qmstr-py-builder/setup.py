4491ef1b86f81612b77fdf9ecfc42358cc8bbf42
Markus Herpich <markus@endocode.com>
2019-09-05 13:20:53 +0200

    Add scaffold for qmstr-py-builder
    Add the scaffold to be filled with the actual implementation.

---
+++ modules/builders/qmstr-py-builder/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+import os
+
+setup(
+    name='qmstr-py-builder',
+    version=os.environ["QMSTR_VERSION"],
+    description='QMSTR Python Builder',
+    url='http://qmstr.org',
+    license='GPLv3',
+
+    packages=['qmstrpybuilder'],
+    install_requires=["pyqmstr=={}".format(os.environ["QMSTR_VERSION"]), 'docopt'],
+    entry_points={
+        'console_scripts': [
+            'qmstr-py-builder = qmstrpybuilder.__main__:main',
+        ],
+    },
+
+)
