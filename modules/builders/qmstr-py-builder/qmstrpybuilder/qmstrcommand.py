0bf010bae5b583cd5a71be98d69ca56be4a49f46
Markus Herpich <markus@endocode.com>
2019-09-09 12:07:20 +0200

    WIP [py builder] use external dir for building
    keep the source dir clean
    
    TODO use python's mktemp to create a temp dir for building

--- modules/builders/qmstr-py-builder/qmstrpybuilder/qmstrcommand.py
+++ modules/builders/qmstr-py-builder/qmstrpybuilder/qmstrcommand.py
@@ -21,18 +21,5 @@ class QMSTRCommand(Command):
     def run(self):
         """runner"""
 
-        # Ensure metadata is up-to-date
-        self.reinitialize_command('build_py', inplace=0)
-        self.run_command('build_py')
-        bpy_cmd = self.get_finalized_command("build_py")
-        build_path = pkg_resources.normalize_path(bpy_cmd.build_lib)
-
-        # Build extensions
-        self.reinitialize_command('egg_info', egg_base=build_path)
-        self.run_command('egg_info')
-
-        self.reinitialize_command('build_ext', inplace=0)
-        self.run_command('build_ext')
-
-        self.reinitialize_command('bdist_dumb', inplace=0, format="gztar", keep_temp=True)
+        self.reinitialize_command('bdist_dumb', inplace=0, format="gztar", keep_temp=True, bdist_dir="/tmp/qmstrsomething")
         self.run_command('bdist_dumb')
