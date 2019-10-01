e18a968b5fa04fc47009a76c9f1dd95d287a9422
Markus Herpich <markus@endocode.com>
2019-10-01 17:18:34 +0200

    [python] build python wheels
    Build wheel packages from python modules for easier installation.

--- lib/pyqmstr/setup.py
+++ lib/pyqmstr/setup.py
@@ -1,13 +1,14 @@
 from setuptools import setup, find_packages
 import os
 
+
 setup(
     name='pyqmstr',
     version=os.environ["QMSTR_VERSION"],
     description='Interface with qmstr-master from python',
     url='http://qmstr.org',
     license='GPLv3',
-
     packages=find_packages(exclude=['tests']),
-    install_requires=['grpcio', 'protobuf'],
+    install_requires=["grpcio=={}".format(
+        os.environ["GRPCIO_VERSION"]), 'protobuf'],
 )
