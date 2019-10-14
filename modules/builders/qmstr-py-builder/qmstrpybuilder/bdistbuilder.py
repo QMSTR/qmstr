67421b96fa463be614afe49d46e5d9d231dcd73f
Markus Herpich <markus@endocode.com>
2019-10-14 11:53:15 +0200

    [pybuilder] connect python bytecode
    Connect python source files to corresponding bytecode files.

--- modules/builders/qmstr-py-builder/qmstrpybuilder/bdistbuilder.py
+++ modules/builders/qmstr-py-builder/qmstrpybuilder/bdistbuilder.py
@@ -25,5 +25,33 @@ class BdistBuilder(QMSTR_Builder):
         file_list = get_files_list(self.temp_dir)
         logging.warn("collected files %s", file_list)
         file_nodes = [new_file_node(f, hash=True) for f in file_list]
+        BdistBuilder.connect_bytecode(file_nodes)
         pkg_node = new_package_node(name, version, file_nodes)
         self.send_package(pkg_node)
+
+    @staticmethod
+    def connect_bytecode(file_nodes):
+        bytecode_nodes = [f for f in file_nodes if f.path.endswith(".pyc")]
+        target_source = dict()
+        for bcfn in bytecode_nodes:
+            directory, filename = os.path.split(bcfn.path)
+            if directory.endswith("__pycache__"):
+                directory = os.path.dirname(directory)
+            
+            filenameparts = filename.split('.')
+            if len(filenameparts) > 2:
+                boundary = -2
+            else:
+                boundary = -1
+            source_filename = os.path.join(directory, *filenameparts[:boundary])
+            source_filename = source_filename + ".py"
+            target_source[source_filename] = bcfn
+
+        source_nodes = [f for f in file_nodes if f.path.endswith("py")]
+        for scfn in source_nodes:
+            try:
+                target = target_source[scfn.path]
+                target.derivedFrom.append(scfn)
+            except KeyError:
+                logging.warn("no bytecode for %s found", scfn.path)
+        
