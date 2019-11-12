37a18e069c3f611856a2c2752d136c3bb40d2ca7
Markus Herpich <markus@endocode.com>
2019-11-12 12:43:56 +0100

    Fix testalyzer

--- modules/analyzers/test-analyzer/main.go
+++ modules/analyzers/test-analyzer/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"log"
 	"os"
@@ -11,7 +10,6 @@ import (
 	"github.com/QMSTR/qmstr/lib/go-qmstr/analysis"
 	"github.com/QMSTR/qmstr/lib/go-qmstr/master"
 	"github.com/QMSTR/qmstr/lib/go-qmstr/module"
-	"github.com/QMSTR/qmstr/lib/go-qmstr/service"
 	"github.com/QMSTR/qmstr/lib/go-qmstr/tester"
 )
 
@@ -20,7 +18,7 @@ const (
 )
 
 var (
-	pkgNode         *service.PackageNode
+	pkgNode         *module.PackageNodeProxy
 	tests           []string
 	testfunction    func(*testing.T)
 	expectedTargets []string
@@ -47,40 +45,38 @@ func (testanalyzer *TestAnalyzer) Configure(configMap map[string]string) error {
 }
 
 func (testanalyzer *TestAnalyzer) Analyze(masterClient *module.MasterClient, token int64) error {
-	pkgNodeStream, err := masterClient.CtrlSvcClient.GetPackageNode(context.Background(), &service.PackageNode{})
+	pkgNodes, err := masterClient.GetPackageNodes()
 	if err != nil {
 		return err
 	}
 
-	pkgNode, err = pkgNodeStream.Recv()
-	if err != nil {
-		return err
-	}
-
-	// Run tests for package node
-	for _, test := range tests {
-		if test == "TestPackageNode" {
-			testfunction = TestPackageNode
-		} else if test == "TestCalcBuildGraph" {
-			expectedTargets = []string{"Calculator/calc", "Calculator/libcalc.so", "Calculator/libcalc.a", "Calculator/calcs"}
-			testfunction = TestBuildGraph
-		} else if test == "TestCurlBuildGraph" {
-			expectedTargets = []string{"curl/build/src/curl", "curl/build/lib/libcurl.so"}
-			testfunction = TestBuildGraph
-		} else {
-			log.Printf("Unknown test. Please check the test name provided in the configuration.")
-			os.Exit(master.ReturnAnalyzerFailed)
+	for _, pnp := range pkgNodes {
+		pkgNode = pnp
+		// Run tests for package node
+		for _, test := range tests {
+			if test == "TestPackageNode" {
+				testfunction = TestPackageNode
+			} else if test == "TestCalcBuildGraph" {
+				expectedTargets = []string{"Calculator/calc", "Calculator/libcalc.so", "Calculator/libcalc.a", "Calculator/calcs"}
+				testfunction = TestBuildGraph
+			} else if test == "TestCurlBuildGraph" {
+				expectedTargets = []string{"curl/build/src/curl", "curl/build/lib/libcurl.so"}
+				testfunction = TestBuildGraph
+			} else {
+				log.Printf("Unknown test. Please check the test name provided in the configuration.")
+				os.Exit(master.ReturnAnalyzerFailed)
+			}
+			testSuite := []testing.InternalTest{
+				{
+					Name: test,
+					F:    testfunction,
+				},
+			}
+			t := &tester.DummyTestDeps{}
+			testing.MainStart(t, testSuite, nil, nil).Run()
 		}
-		testSuite := []testing.InternalTest{
-			{
-				Name: test,
-				F:    testfunction,
-			},
-		}
-		t := &tester.DummyTestDeps{}
-		testing.MainStart(t, testSuite, nil, nil).Run()
-	}
 
+	}
 	return nil
 }
 
@@ -89,20 +85,30 @@ func (testanalyzer *TestAnalyzer) PostAnalyze() error {
 }
 
 func TestPackageNode(t *testing.T) {
-	if len(pkgNode.Targets) < 1 {
+	targets, err := pkgNode.GetTargets()
+	if err != nil {
+		t.Logf("test failed: %v", err)
+		t.Fail()
+	}
+	if len(targets) < 1 {
 		t.Logf("Package node '%v' is not connected to any linked targets", pkgNode.Name)
 		t.Fail()
 	}
 }
 
 func TestBuildGraph(t *testing.T) {
-	if len(pkgNode.Targets) < 2 {
+	targets, err := pkgNode.GetTargets()
+	if err != nil {
+		t.Logf("test failed: %v", err)
+		t.Fail()
+	}
+	if len(targets) < 2 {
 		t.Logf("Package node '%v' is not connected to all the configured linked targets", pkgNode.Name)
 		t.Fail()
 	} else {
 		for _, expectedTarget := range expectedTargets {
 			var found bool
-			for _, target := range pkgNode.Targets {
+			for _, target := range targets {
 				if expectedTarget == target.Path {
 					found = true
 					t.Logf("Package node %v is connected to %v", pkgNode.Name, target.Path)
